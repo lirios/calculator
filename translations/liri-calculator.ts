c3f58abc04f77dc422c3af36621cc91a9fe1385b
Liri CI <ci@liri.io>
2020-04-19 22:01:55 +0000

    Automatic merge of Transifex translations
    [ci skip]

--- translations/liri-calculator.ts
+++ translations/liri-calculator.ts
@@ -4,7 +4,7 @@
 <context>
     <name>CalculationLine</name>
     <message>
-        <location filename="../ui/CalculationLine.qml" line="72"/>
+        <location filename="../src/ui/CalculationLine.qml" line="72"/>
         <source>Error</source>
         <translation type="unfinished"></translation>
     </message>
@@ -12,37 +12,37 @@
 <context>
     <name>CalculationZone</name>
     <message>
-        <location filename="../ui/CalculationZone.qml" line="56"/>
+        <location filename="../src/ui/CalculationZone.qml" line="56"/>
         <source>Help</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/CalculationZone.qml" line="64"/>
+        <location filename="../src/ui/CalculationZone.qml" line="64"/>
         <source>Open file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/CalculationZone.qml" line="74"/>
+        <location filename="../src/ui/CalculationZone.qml" line="74"/>
         <source>Save file</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/CalculationZone.qml" line="82"/>
+        <location filename="../src/ui/CalculationZone.qml" line="82"/>
         <source>Advanced mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/CalculationZone.qml" line="90"/>
+        <location filename="../src/ui/CalculationZone.qml" line="90"/>
         <source>Close advanced mode</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/CalculationZone.qml" line="98"/>
+        <location filename="../src/ui/CalculationZone.qml" line="98"/>
         <source>Toggle history</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/CalculationZone.qml" line="106"/>
+        <location filename="../src/ui/CalculationZone.qml" line="106"/>
         <source>Toggle expanded</source>
         <translation type="unfinished"></translation>
     </message>
@@ -50,7 +50,7 @@
 <context>
     <name>FileHandler</name>
     <message>
-        <location filename="../filehandler/filehandler.cpp" line="85"/>
+        <location filename="../src/filehandler/filehandler.cpp" line="85"/>
         <source>Cannot save: </source>
         <translation type="unfinished"></translation>
     </message>
@@ -58,12 +58,12 @@
 <context>
     <name>Main</name>
     <message>
-        <location filename="../ui/Main.qml" line="173"/>
+        <location filename="../src/ui/Main.qml" line="173"/>
         <source>Discard unsaved?</source>
         <translation type="unfinished"></translation>
     </message>
     <message>
-        <location filename="../ui/Main.qml" line="218"/>
+        <location filename="../src/ui/Main.qml" line="218"/>
         <source>saved</source>
         <translation type="unfinished"></translation>
     </message>
