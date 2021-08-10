09483012cd7e2f7d1220a4077e13261cbf3bf731
Liri CI <ci@liri.io>
2021-08-10 00:41:08 +0000

    Automatic merge of Transifex translations
    [ci skip]

---
+++ translations/liri-calculator_de.ts
@@ -0,0 +1,69 @@
+<?xml version="1.0" ?><!DOCTYPE TS><TS language="de" version="2.1">
+<context>
+    <name>CalculationLine</name>
+    <message>
+        <location filename="../src/ui/CalculationLine.qml" line="72"/>
+        <source>Error</source>
+        <translation>Fehler</translation>
+    </message>
+</context>
+<context>
+    <name>CalculationZone</name>
+    <message>
+        <location filename="../src/ui/CalculationZone.qml" line="56"/>
+        <source>Help</source>
+        <translation>Hilfe</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/CalculationZone.qml" line="64"/>
+        <source>Open file</source>
+        <translation>Datei öffnen</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/CalculationZone.qml" line="74"/>
+        <source>Save file</source>
+        <translation>Datei speichern</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/CalculationZone.qml" line="82"/>
+        <source>Advanced mode</source>
+        <translation>Erweiterter Modus</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/CalculationZone.qml" line="90"/>
+        <source>Close advanced mode</source>
+        <translation>Erweiterten Modus schließen</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/CalculationZone.qml" line="98"/>
+        <source>Toggle history</source>
+        <translation>Verlauf umschalten</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/CalculationZone.qml" line="106"/>
+        <source>Toggle expanded</source>
+        <translation type="unfinished"/>
+    </message>
+</context>
+<context>
+    <name>FileHandler</name>
+    <message>
+        <location filename="../src/filehandler/filehandler.cpp" line="85"/>
+        <source>Cannot save: </source>
+        <translation>Kann nicht speichern: </translation>
+    </message>
+</context>
+<context>
+    <name>Main</name>
+    <message>
+        <location filename="../src/ui/Main.qml" line="173"/>
+        <source>Discard unsaved?</source>
+        <translation>Ungespeichertes verwerfen?</translation>
+    </message>
+    <message>
+        <location filename="../src/ui/Main.qml" line="218"/>
+        <source>saved</source>
+        <translation>gespeichert</translation>
+    </message>
+</context>
+</TS>
